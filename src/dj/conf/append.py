f835e452c123fa5088fc2005238ebfa61e3ef3a9
Brant <brant.steen@gmail.com>
2011-12-30 14:42:12 -0500

    Settings dance configs and apache wsgi template

--- src/dj/conf/append.py
+++ src/dj/conf/append.py
@@ -6,8 +6,7 @@ These settings usually fall into one of these categories:
     2) setting that requires the import of some other specific setting (e.g. DEBUG)
 
 """
-
-from dj.settings import DEBUG, INSTALLED_APPS, MIDDLEWARE_CLASSES, STATIC_URL
+from dj.settings import DEBUG, INSTALLED_APPS, MIDDLEWARE_CLASSES, STATIC_URL, THE_THEME, PROJECT_PATH
 
 if STATIC_URL.startswith("/") and not STATIC_URL.startswith("//"):
     MEDIABRUTE_CACHE_BASE_URL = "/"
@@ -15,4 +14,12 @@ if STATIC_URL.startswith("/") and not STATIC_URL.startswith("//"):
     JS_DIR = "static/js"
     MEDIABRUTE_CSS_URL_PATH = "css"
     MEDIABRUTE_JS_URL_PATH = "js"
-
+    
+    STATICFILES_DIRS = (
+        # Put strings here, like "/home/html/static" or "C:/www/django/static".
+        # Always use forward slashes, even on Windows.
+        # Don't forget to use absolute paths, not relative paths.
+        PROJECT_PATH + "/assets/" + THE_THEME + "/static/",
+    )
+    
+    STATIC_ROOT = PROJECT_PATH + "/assets/" + THE_THEME + "/"
--- src/dj/conf/base.py
+++ src/dj/conf/base.py
@@ -65,7 +65,7 @@ MEDIA_ROOT = PROJECT_PATH + "/assets/uploads/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = PROJECT_PATH + "/assets/" + THE_THEME + "/"
+STATIC_ROOT = PROJECT_PATH + "/assets/" + THE_THEME + "/static/"
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -81,7 +81,6 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    PROJECT_PATH + "/assets/" + THE_THEME + "/static/",
 )
 
 # List of finder classes that know how to find static files in
