0c52d62e5efb7de769e29a79ef8d21ce88745761
Brant Steen <brant.steen@gmail.com>
2012-06-25 12:25:47 -0400

    All sorts of good bits

--- README.md
+++ README.md
@@ -3,11 +3,9 @@ Base Django Project
 
 ## Get up and running
 From the root of this project...
+	python ./gogo.py
+You'll need either setuptools or virtualenv already setup for your python environment
 
-1. **copy:** ./dj/settings_template.py to ./dj/settings.py
-2. **in settings.py**, set DEBUG to True (if you are going to start developing stuff)
-3. **run:** python ./manage.py syncdb
-4. **run:** python ./manage.py runserver 8001
 
 ## Customize settings
 - Customize *your* settings in ./dj/settings.py
--- gogo.py
+++ gogo.py
@@ -1,11 +1,81 @@
 #!/usr/bin/python
 
 import os
-import subprocess
+import shutil
 
-def setup_env():    
-    os.system("virtualenv env;. ./env/bin/activate; pip install -r ./requirements.txt")
+
+def do_stuff():
+    """
+    Run a bunch of stuff in sequence
+    """
+    setup_env()
+    initial_syncdb()
+    run_server()
+
+
+def setup_env():
+    """
+    Run a bunch of stuff to setup the environment
+    """
+    print "--- Setting up environment ---"
+    env = "env"
+    my_dir = os.path.abspath( os.path.dirname(__file__) )    
+    conf = os.path.join(my_dir, "dj/settings.py")
+    conf_template = os.path.join(my_dir, "dj/settings_template.py")
+    env_path = os.path.join(my_dir, "env")
+    
+    commands = ["virtualenv env",
+                ". ./env/bin/activate",]
+    
+    if os.path.isdir(env_path):
+        print "--- Environment found, will upgrade packages ---"
+        commands.append("pip install --upgrade -r ./requirements.txt")
+    else:
+        print "--- Did not find environment ---"
+        print "--- Will create environment from scratch ---"
+        commands.append("pip install -r ./requirements.txt")
+    
+    
+    
+    if not os.path.isfile(conf):
+        print "--- Did not find settings file ---"
+        print "--- Will create settings file base on template ---"
+        shutil.copyfile(conf_template, conf)
+    
+    run_commands(commands)
+
+
+def initial_syncdb():
+    """
+    Initial decision making for a fresh environment
+    """
+    from dj import settings
+    if not os.path.isfile(settings.DATABASES["default"]["NAME"]):
+        syncdb()
+
+
+def syncdb():
+    """
+    Sync the database
+    """
+    commands = [". ./env/bin/activate",
+                "python manage.py syncdb",
+                ]
+    print "--- Syncing database ---"
+    run_commands(commands)
+    
+    
+def run_server():
+    print "--- Running server ---"
+    commands = [". ./env/bin/activate",
+                "python ./manage.py runserver 8001",
+                ]
+    run_commands(commands)
+
+
+def run_commands(commands):
+    os.system(";".join(commands))
     
     
 if __name__ == "__main__":
-    setup_env()
+    do_stuff()
