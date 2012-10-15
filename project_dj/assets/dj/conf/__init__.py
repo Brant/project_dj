f20da95b408815f61f45b97f60a699a932d293f6
Brant Steen <brant.steen@gmail.com>
2012-10-15 19:45:00 -0400

    Still Refactoring

--- project_dj/assets/dj/conf/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Global, project configurations
-
-Configurations defined here should work (in some way) accross environments
-"""
--- project_dj/assets/dj/conf/tests_template.py
+++
@@ -1,9 +0,0 @@
-TEST_APPS = ("django_nose", )
-
-TEST_RUNNER = "django_nose.runner.NoseTestSuiteRunner"
-
-NOSE_ARGS = ["--with-xcoverage", "--cover-inclusive", "--with-xunit", "--exe", "--verbosity=3"]
-NOSE_PLUGINS = [
-    'nosexcover.XCoverage',
-    "nose_exclude.NoseExclude"
-]
