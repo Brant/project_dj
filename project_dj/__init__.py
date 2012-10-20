290d89121f69985cc1c6da635e060ee7e33eb21d
Brant Steen <brant.steen@gmail.com>
2012-10-20 12:45:25 -0400

    structuring assets for different commands/functions

--- project_dj/__init__.py
+++ project_dj/__init__.py
@@ -4,4 +4,4 @@ Project DJ
 import os
 from project_dj.util import recursive_file_gen
 
-PROJECT_DJ_ASSETS = os.path.join(os.path.abspath( os.path.dirname(__file__) ), "assets") 
+DJ_NEW_PROJECT_ASSETS = os.path.join(os.path.abspath( os.path.dirname(__file__) ), "assets", "new_project") 
