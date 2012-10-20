338a03633e602403c38f9dad82e36404140d92c9
Brant Steen <brant.steen@gmail.com>
2012-10-20 11:04:46 -0400

    urls

--- project_dj/assets/urls.py
+++ project_dj/assets/urls.py
@@ -18,6 +18,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
+    (r'^favicon\.png', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.png'}),
     (r'^apple-touch-icon\.png', 'django.views.generic.simple.redirect_to', {'url': '/static/apple-touch-icon.png'}),
 )
 
